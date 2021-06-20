bbcd3e04b7b15d065f33af29312537303ca74b07
Myron <myronmax_seelen@live.nl>
2021-06-20 18:33:37 +0200

    Done

--- index.php
+++ index.php
@@ -1,4 +1,3 @@
-
 <!doctype html>
 <html lang="en">
 <head>
@@ -11,14 +10,52 @@
 </head>
 <body>
     <a-scene>
-        <a-box position="-0.048 0.0 -5.027" rotation="0 45 0" color="#4CC3D9"></a-box>
-        <a-sphere position="0 1.25 -5" radius="1.25" color="#FFFFF"></a-sphere>
-        <a-sphere position="0 3.5 -5" radius="1.25" color="#ff6600"></a-sphere>
-        <a-sphere position="11.882 9.033 -24.456" scale="4 4 4" radius="1.25" color="#ff6600"></a-sphere>
-        <a-box position="-2.399 1.8 -5.106" scale="1.0 1.0 8.0" height="0.100" rotation="0 25 0" color="#04be89"></a-box>
-        <a-cylinder position="2.860 0.75 -5.979" scale="1 2.1 1" radius="0.5" height="1.5" color="#FFC65D"></a-cylinder>
-        <a-plane position="0 0 -4" rotation="-90 0 0" scale="2 2 2" width="40" height="40" color="#7BC8A4"></a-plane>
-        <a-sky color="#399bff"></a-sky>
+        <a-box
+               position="-0.048 0.0 -5.027"
+               rotation="0 45 0"
+               color="#4CC3D9"
+        ></a-box>
+        <a-sphere
+                position="0 1.25 -5"
+                radius="1.25"
+                color="#FFFFF"
+        ></a-sphere>
+        <a-sphere
+                position="0 3.5 -5"
+                radius="1.25"
+                color="#ff6600"
+        ></a-sphere>
+        <a-sphere
+                position="11.882 9.033 -24.456"
+                scale="4 4 4"
+                radius="1.25"
+                color="#ff6600"
+        ></a-sphere>
+        <a-box
+                position="-2.399 1.8 -5.106"
+                scale="1.0 1.0 8.0"
+                height="0.100"
+                rotation="0 25 0"
+                color="#04be89"
+        ></a-box>
+        <a-cylinder
+                position="2.860 0.75 -5.979"
+                scale="1 2.1 1"
+                radius="0.5"
+                height="1.5"
+                color="#FFC65D"
+        ></a-cylinder>
+        <a-plane
+                position="0 0 -4"
+                rotation="-90 0 0"
+                scale="2 2 2"
+                width="40"
+                height="40"
+                color="#7BC8A4"
+        ></a-plane>
+        <a-sky
+                color="#399bff"
+        ></a-sky>
     </a-scene>
 </body>
 </html>
